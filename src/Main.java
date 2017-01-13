39c9d45b764800addea62ac30ca1a792b84cd3e9
FreakySheldon <thagen@aggregatgames.com>
2017-01-13 20:55:12 +0100

    Added Comments

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,9 @@ import Arkanoid.*;
 
 public class Main {
 	
+	//called uppon launching the program
 	public static void main(String[] args) {
+		//Create  a new window and display the game
 		JFrame frame = new JFrame("Arkanoid");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLayout(new BorderLayout());
